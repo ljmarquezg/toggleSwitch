c27c84d3a9fb33db12c9d3a634f73d6c4d31e32d
Luis <luisjose.marquezg@seidor.es>
2020-03-30 18:38:22 +0200

    Included hover effects

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ window.onload = () => {
     const getUsers = () => {
         var xhr = new XMLHttpRequest();
         fetch('https://randomuser.me/api/?results=20')
-            .then(function (response) {
+            .then((response) => {
                 return response.json();
             })
-            .then(function (myJson) {
+            .then((myJson) => {
                 console.log(myJson.results);
                 let target = document.querySelector('.content');
                 myJson.results.map((user) => {
@@ -15,7 +15,11 @@ window.onload = () => {
                         info = document.createElement('div'),
                         username = document.createElement('h3'),
                         email = document.createElement('h4'),
-                        gender = document.createElement('p');
+                        gender = document.createElement('p'),
+                        location = document.createElement('div'),
+                        city = document.createElement('p'),
+                        country = document.createElement('p'),
+                        postCode = document.createElement('p');
 
                     cardContainer.classList.add('card');
 
@@ -23,27 +27,40 @@ window.onload = () => {
                     cardImg.src = user.picture.thumbnail;
 
                     gender.classList.add('gender');
-                    gender.innerHTML = user.gender;
+                    gender.innerHTML = '<strong>Gender: </strong>' + user.gender;
 
                     username.innerHTML = user.name.title + ' ' + user.name.first + ' ' + user.name.last
-                    info.classList.add('info');
+                    info.classList.add('info', 'small');
 
                     email.innerHTML = user.email;
 
                     cardContainer.appendChild(cardImg);
                     cardContainer.appendChild(info);
+
                     info.appendChild(username);
-
                     info.appendChild(gender);
-                    target.appendChild(cardContainer);
 
-                    let styleTag = document.createElement('style');
-                    for (let i = 1; i < myJson.results.length; i++) {
-                        let style = '.enableCSS .card{{&:nth-child(' + i + '){animation - delay: ' + (i * 0.5) + 's;}}}';
-                    }
-                    document.getElementsByTagName('head')[0].appendChild(styleTag);
-                });
-            });
+                    location.classList.add('location', 'd-flex')
+                    city.innerHTML = '<strong>City: </strong>' + user.location.city
+                    country.innerHTML = '<strong>Country:</strong>' + user.location.country;
+                    postCode.innerHTML = '<strong>Post Code: </strong>' + user.location.postcode;
+
+                    location.appendChild(city);
+                    location.appendChild(country);
+                    location.appendChild(postCode);
+
+                    info.appendChild(location);
+                    target.appendChild(cardContainer);
+                })
+                return myJson.results.length;
+
+            }).then((totalItems) => {
+                let styleTag = document.createElement('style');
+                for (let i = 1; i <= totalItems; i++) {
+                    styleTag.innerHTML += '.enableCSS .card:nth-child(' + i + '){animation-delay: ' + (i * 0.5) + 's;}';
+                }
+                document.getElementsByTagName('head')[0].appendChild(styleTag);
+            });;
     }
 
     const enableCss = (toggle) => {
